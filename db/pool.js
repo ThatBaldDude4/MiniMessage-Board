e35db0e0f508db7ae2847ffc2be6bf71ef906f5d
ThatBaldDude4 <taylorv0004@gmail.com>
2026-08-21 23:02:29 -0500

    update connection URL

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,5 @@ const { Pool } = require("pg");
 const {HOST, USER, DATABASE, PASSWORD, PORT} = process.env;
 
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.MSG_URL,
 });
