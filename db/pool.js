1b4ce8601f0066028249c5181ca85eadc56f72eb
ThatBaldDude4 <taylorv0004@gmail.com>
2026-08-21 22:52:47 -0500

    remove .env load for prod build

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,5 @@
 const { Pool } = require("pg");
 
-const { loadEnvFile } = require('node:process');
-loadEnvFile();
 const {HOST, USER, DATABASE, PASSWORD, PORT} = process.env;
 
 module.exports = new Pool({
